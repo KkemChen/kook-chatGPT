9eb571e03f5df1ffa92a9134a4ae81f2b29a27d1
KkemChen <44298896+qq704361748@users.noreply.github.com>
2023-03-12 18:20:45 +0800

    Update conversation.ts

--- src/commands/chat/conversation.ts
+++ src/commands/chat/conversation.ts
@@ -71,7 +71,7 @@ export async function run(session: BaseSession, prefix: boolean): Promise<void> 
                     "elements": [
                         {
                             "type": "image",
-                            "src": "https://img.kookapp.cn/assets/2023-03/iNYyZNQk7704g04g.png"
+                            "src": "https://img.kookapp.cn/assets/2023-03/DhZKcWJ2KD07c06m.png"
                         },
                         {
                             "type": "plain-text",
