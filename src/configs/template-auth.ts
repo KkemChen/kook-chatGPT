68d4cacc06827f574ca641adcb047dcf84e3a3cd
Kkem Chen <kkemchen@qq.com>
2023-03-12 12:41:17 +0800

    init

---
+++ src/configs/template-auth.ts
@@ -0,0 +1,9 @@
+export default {
+    useWebhook: false,
+    khlport: 6000,
+    khlkey: 'encrypt key here',
+    khltoken: 'token here',  //填入KOOK机器人token
+    khlverify: 'verify token here',
+
+    openAIKey: 'api key'  //填入chatgpt apikey
+};
